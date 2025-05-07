3348ad91cb4310192e02d153d34d42caf23d48e7
VedantChaskar <chaskarvedant1@gmail.com>
2025-05-07 01:01:19 -0700

    Add deleteRound function #1

--- scripts/roundsMode.js
+++ scripts/roundsMode.js
@@ -217,6 +217,20 @@ const thisRound = document.getElementById("r-" + GlobalUserData.rounds[rowIndex]
 writeRoundToTable(thisRound,rowIndex);
 }
 
+
+/*************************************************************************
+ * @function deleteRound
+ * @desc
+ * Deletes a round from the "Rounds" table and from local storage
+ * @param roundId -- the unique id of the round to be deleted
+ * @returns -- true if round could be deleted, false otherwise
+ *************************************************************************/
+function deleteRound(roundId) {
+  GlobalUserData.rounds = GlobalUserData.rounds.filter(function (round) {
+      return round.roundNum !== roundId;
+  });
+}
+
 /*************************************************************************
  * @function confirmDelete
  * @desc
